d0d0f9419c7d423e9664d1baa83ee87aeb5403a3
Suhib Yaser Abed <253351588+suhaib205@users.noreply.github.com>
2026-02-10 15:14:03 +0200

    Update Supabase config with additional fieldssupabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,5 +1,9 @@
 window.SUPABASE_CONFIG = {
   url: "https://ziozomnceumhdwzccwke.supabase.co",
   anonKey: "sb_publishable_RzTql6NRNONpdQsXm_MHNg_T0sv2PVh",
-  storageBucket: "product-images"
+  bucket: "product-images",
+
+  brandName: "Suhaib Store",
+  instagramUrl: "https://instagram.com/suhaib._.store",
+  whatsappNumber: "966500000000" // حط رقمك مع كود الدولة بدون +
 };
