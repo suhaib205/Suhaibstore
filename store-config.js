c728bca0a0f1390b80a13fd37d47b74cffe2ad5c
Suhib Yaser Abed <253351588+suhaib205@users.noreply.github.com>
2026-02-11 13:14:56 +0200

    Delete store-config.js

--- store-config.js
+++
@@ -1,7 +0,0 @@
-// إعدادات المتجر (عدّلها)
-window.STORE_CONFIG = {
-  storeName: "Suhaib Store",
-  whatsappNumber: "970XXXXXXXXX", // بدون +
-  instagramUrl: "https://instagram.com/suhaib._.store",
-  defaultLang: "ar",
-};
